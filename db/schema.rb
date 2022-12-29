aaa1cce648e0d117de555745b8cca747e5f755de
Laura Palagano <69463821+LauraEliza19@users.noreply.github.com>
2022-12-29 14:28:39 -0300

    Deleting unnecessary data base tables

--- db/schema.rb
+++ db/schema.rb
@@ -23,14 +23,6 @@ ActiveRecord::Schema.define(version: 2022_12_13_144023) do
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end
 
-  create_table "meetings", force: :cascade do |t|
-    t.string "name"
-    t.datetime "start_time"
-    t.datetime "end_time"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
