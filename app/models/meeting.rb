547fa6b0d6a696388f03c9f878189f709601cf8a
Laura Palagano <69463821+LauraEliza19@users.noreply.github.com>
2023-01-16 08:51:25 -0300

    Delete meeting.rb

--- app/models/meeting.rb
+++
@@ -1,7 +0,0 @@
-class Meeting < ApplicationRecord
-    belongs_to :user
-    # def Index
-    #     start_time = params.fetch(:start_time, Date.today).to_date
-    #     @meetings = Meeting.where(starts_at: start_date.beginning_of_month.beginning_of_week..start_date.end_of_month.end_of_week)
-    # end 
-end
