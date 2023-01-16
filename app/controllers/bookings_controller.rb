78c58efa9eb3e28d5e60334a05334bbfa36d3fc5
Laura Palagano <69463821+LauraEliza19@users.noreply.github.com>
2023-01-16 08:43:47 -0300

    Update bookings_controller.rb

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -3,7 +3,6 @@ class BookingsController < ApplicationController
   
 #   before_action :authenticate_user!, except: [:index, :show]
   # GET /bookings or /bookings.json
-
    def index
     @bookings = Booking.all
     @currentTime = Time.zone.now
@@ -15,17 +14,14 @@ class BookingsController < ApplicationController
   end
 
   # GET /bookings/new
-  def new
-        
-        @booking = Booking.new
+  def new        
+        @booking = current_user.bookings.build
+        # @booking = Booking.new
         @datePass = params[:star_date]
         @timePass = params[:start_time]
         @booking.star_date = @datePass
         @booking.start_time = @timePass
-        @reuniao = false
-
-        @currentTime = Time.zone.now
-    
+        @currentTime = Time.zone.now        
   end
 
   # GET /bookings/1/edit
@@ -38,12 +34,16 @@ class BookingsController < ApplicationController
   # POST /bookings or /bookings.json
   def create
     # @booking.user_id=1
-    @booking = Booking.new(booking_params)
+    # @booking = Booking.new(booking_params)
+    @booking = current_user.bookings.build(booking_params)    
     respond_to do |format|
-        
-        format.html { redirect_to bookings_path, notice: "Reserva feita com sucesso!"}
-        format.json { render 'bookings/success', notice: "Reserva feita com sucesso"}
-    end
+        if @booking.save
+            format.html { redirect_to bookings_path, notice: "Reserva feita com sucesso!" }
+        else
+            format.html { render :index, status: :unprocessable_entity, notice:"Não foi possível realizar a reserva!" }
+            format.json { render json: @booking.errors, status: :unprocessable_entity, notice:"Não foi possível realizar a reserva!" }
+        end
+      end
   end
 
   # PATCH/PUT /bookings/1 or /bookings/1.json
@@ -76,7 +76,6 @@ class BookingsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def booking_params
-      params.require(:booking).permit(:description, :star_date, :start_time, :user)
-    #   params.require(:booking).permit(:description, :start_time, :end_time, :star_date)
+      params.require(:booking).permit(:description, :star_date, :start_time, :user.name)
     end
 end
