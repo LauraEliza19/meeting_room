e3caa705029212cecaeee61aa1b5646391a4a9ec
Laura Palagano <69463821+LauraEliza19@users.noreply.github.com>
2023-01-16 08:42:43 -0300

    Update rules - only logged in users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,36 +1,17 @@
 class ApplicationController < ActionController::Base
-    # protect_from_forgery with: :exception
-    # include SessionsHelper
-    # Prevent CSRF attacks by raising an exception.
-    # For APIs, you may want to use :null_session instead.
-    # protect_from_forgery with: :exception
-    include SessionsHelper
-    # def new 
-    #     respond_to do |format|
-    #         format.html
-    #         format.js
-    #     end
-    # end
-    protect_from_forgery
-
-# todos os metodos daqui em diante serão privados
-private 
-
-  def logged_in?
-    current_user.present? # se esta presente, se tem valor
-  end
-
-  # current_user vai ser o usuario de acordo com o :user_id armazenado em sessão
-
-  def current_user
-    @current_user ||= session[:user_id] && User.find_by_id(session[:user_id])
-  end
-
-  def require_logged_user
-    return if logged_in?
-
-    redirect_to login_path, :notice => "É necessario estar logado para acessar"
-  end
-
-    
+    helper_method :current_user, :logged_in?
+    def current_user
+      @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    end
+     
+    def logged_in?
+      !!current_user
+    end
+     
+    def require_user
+      if !logged_in?
+        flash[:alert] = "Você necessita estar logado."
+        redirect_to login_path
+      end
+    end
 end
