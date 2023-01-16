abd70ecc5f00dc527d8a2f505a35c9183e18c1ae
Laura Palagano <69463821+LauraEliza19@users.noreply.github.com>
2023-01-16 08:50:43 -0300

    Update users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,36 +3,26 @@ class UsersController < ApplicationController
     def new
         @user = User.new
         respond_to do |format|
-
             format.html
-
             format.js
-
         end
     end
 
     def create
         @user = User.new(user_params)
-        respond_to do |format|
-
-            format.html
-
-            format.js
-
-        end
         if @user.save
-                    redirect_to  bookings_path, notice: "Usuário foi criado com sucesso!"
-                    #tire o método de comentário quando criar o helper.
-                    #Usuário depois de cadastrar-se acessa o sistema automaticamente
-            else
-                render action: :new
+          flash[:notice] = "Usuário #{@user.name} Criado!"
+          redirect_to sessions_new_path
+        else
+          render 'new'
         end
-    end
-    
-    private
-    def user_params
+      end
+      
+     private
+      
+      def user_params
         params.require(:user).permit(:name, :email, :password, :password_confirmation)
-    end
+      end
     def index
         @users = User.all
       end
